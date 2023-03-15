7647cbccff6cb094ec92124aa9e6c5a3c024382d
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-03-15 18:04:41 +0100

    installed linters, updated README, and fixed linters errors

--- README.md
+++ README.md
@@ -1,1 +1,135 @@
 # Ruby-on-rails-bootcamp
+<a name="readme-top"></a>
+
+# 📗 Table of Contents
+
+- [📖 About the Project](#about-project)
+  - [🛠 Built With](#built-with)
+  - [🚀 Live Demo](#live-demo)
+- [💻 Getting Started](#getting-started)
+  - [Install](#install)
+- [👥 Authors](#authors)
+- [🔭 Future Features](#future-features)
+- [🤝 Contributing](#contributing)
+- [⭐️ Show your support](#support)
+- [🙏 Acknowledgements](#acknowledgements)
+- [📝 License](#license)
+
+# 📖 Capstone-Project1 <a name="about-project"></a>
+
+> The capstone project is like the evaluation of everything you learnt during the module.Every Module features a Capstone Project, that takes place in the last week. These projects are meant to mimic real-world projects where we must apply what we learned throughout the entire Module, and are built with business specifications. If it is made in a very professional way, this project can be a very good asset in our portfolio.
+
+
+## 🛠 Built With <a name="built-with"></a>
+
+### Tech Stack <a name="tech-stack"></a>
+
+> This project is built using HTML and CSS. 
+
+<details>
+  <summary>HTML</summary>
+  <ul>
+    <li><a href="https://html.com/#What_is_HTML">"index.html" and "about.html"</a></li>
+  </ul>
+</details>
+
+<details>
+  <summary>CSS</summary>
+  <ul>
+    <li><a href="https://html.com/css/#What_is_CSS">"style.css" and "about.css"</a></li>
+  </ul>
+</details>
+
+<details>
+  <summary>Bootstrap</summary>
+  <ul>
+    <li><a href="https://getbootstrap.com/docs/5.2/getting-started/introduction/">Bootstrap</a></li>
+  </ul>
+</details>
+
+<details>
+  <summary>Javascript</summary>
+  <ul>
+    <li><a href="https://www.javascript.com/">"script.js"</a></li>
+  </ul>
+</details>
+
+### Key Features <a name="key-features"></a>
+
+- **No linter errors**
+- **Use of correct GitHub flow**
+- **Documented in a professional way**
+- **Follow best practices for HTML and CSS**
+- **Header and Headline section**
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## 🚀 Live Demo <a name="live-demo"></a>
+
+- [<a href="#" target="_blank">Online version</a>](#)
+
+- [Watch Presentation](#)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## 💻 Getting Started <a name="getting-started"></a>
+
+To get a local copy up and running, follow these steps.
+
+### Prerequisites
+
+In order to run this project you need:
+
+- **Visual Studio Code**
+- **Git Bash**
+
+### Setup
+
+Clone this repository to your desired folder:
+- **cd your-folder**
+- **git clone https://github.com/adeola003/Ruby-on-rails-bootcamp.git**
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## 👥 Authors <a name="authors"></a>
+
+ **Author 1** **Abdel-Khafid SALAOU**
+
+- GitHub: [@githubhandle](https://github.com/adeola003)
+- Twitter: [@twitterhandle](https://twitter.com/khadaf6)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/abdel-khafid-salaou-a18396161)
+**Original design by** **Cindy Shin**
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## 🔭 Future Features <a name="future-features"></a>
+
+- **Build a complete Portfolio website**
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## 🤝 Contributing <a name="contributing"></a>
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## ⭐️ Show your support <a name="support"></a>
+
+If you like this feel free to follow me on github and give this project a star.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## 🙏 Acknowledgments <a name="acknowledgements"></a>
+
+I would like to thank my coding partners, mentors and Microverse for their support in building the project.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## 📝 License <a name="license"></a>
+
+This project is [MIT](./LICENSE.md) licensed.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
--- script.js
+++ script.js
@@ -1,72 +1,71 @@
 // Customized bootstrap nav-btn to transfrom it into X when clicked
 
-const menuBtn = document.querySelector(".navbar-toggler");
+const menuBtn = document.querySelector('.navbar-toggler');
 const openBtn = document.querySelector('.open-btn');
 const closeBtn = document.querySelector('.close-btn');
 
 menuBtn.addEventListener('click', () => {
-    const expanded = menuBtn.getAttribute('aria-expanded');
-    if(expanded === 'true'){
-        openBtn.classList.add('disabled')
-        closeBtn.classList.add('btn-active')
-    }
-    else{
-        openBtn.classList.remove('disabled')
-        closeBtn.classList.remove('btn-active')
-    }
+  const expanded = menuBtn.getAttribute('aria-expanded');
+  if (expanded === 'true') {
+    openBtn.classList.add('disabled');
+    closeBtn.classList.add('btn-active');
+  } else {
+    openBtn.classList.remove('disabled');
+    closeBtn.classList.remove('btn-active');
+  }
 });
 
 // Skpeakers section
 const speakersInfos = [
-    {
+  {
     name: 'Abdel-Khafid SALAOU',
     image: 'assets/speakers/khadaf.jpeg',
     job: 'Software Engineer, CEO of DEOLAcorp',
-    bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto tenetur minima quia laborum delectus unde est dolorem. Distinctio fugiat iusto ad corrupti maxime officia? Benkler studies commons-based peer production'
+    bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto tenetur minima quia laborum delectus unde est dolorem. Distinctio fugiat iusto ad corrupti maxime officia? Benkler studies commons-based peer production',
 
-},
-{
+  },
+  {
     name: 'Faiquot Bounamboulinas SALAOU',
     image: 'assets/speakers/shade.jpg',
     job: 'Software Engineer',
-    bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto tenetur minima quia laborum delectus unde est dolorem. Distinctio fugiat iusto ad corrupti maxime officia? Benkler studies commons-based peer production'
+    bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto tenetur minima quia laborum delectus unde est dolorem. Distinctio fugiat iusto ad corrupti maxime officia? Benkler studies commons-based peer production',
 
-},
-{
+  },
+  {
     name: 'Henary KHURAIJAM',
     image: 'assets/speakers/henary.jpg',
     job: 'Software Engineer',
-    bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto tenetur minima quia laborum delectus unde est dolorem. Distinctio fugiat iusto ad corrupti maxime officia? Benkler studies commons-based peer production'
+    bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto tenetur minima quia laborum delectus unde est dolorem. Distinctio fugiat iusto ad corrupti maxime officia? Benkler studies commons-based peer production',
 
-},
-{
+  },
+  {
     name: 'Ayaba DOUBOGAN',
     image: 'assets/speakers/female2.jpg',
     job: 'Software Engineer',
-    bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto tenetur minima quia laborum delectus unde est dolorem. Distinctio fugiat iusto ad corrupti maxime officia? Benkler studies commons-based peer production'
+    bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto tenetur minima quia laborum delectus unde est dolorem. Distinctio fugiat iusto ad corrupti maxime officia? Benkler studies commons-based peer production',
 
-},
-{
+  },
+  {
     name: 'Gaspard Francko DJOSSOU',
     image: 'assets/speakers/francko.jpg',
     job: 'Software Engineer',
-    bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto tenetur minima quia laborum delectus unde est dolorem. Distinctio fugiat iusto ad corrupti maxime officia? Benkler studies commons-based peer production'
+    bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto tenetur minima quia laborum delectus unde est dolorem. Distinctio fugiat iusto ad corrupti maxime officia? Benkler studies commons-based peer production',
 
-},
-{
+  },
+  {
     name: 'Lola SOUBALOU',
     image: 'assets/speakers/female3.jpg',
     job: 'Software Engineer',
-    bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto tenetur minima quia laborum delectus unde est dolorem. Distinctio fugiat iusto ad corrupti maxime officia? Benkler studies commons-based peer production'
+    bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto tenetur minima quia laborum delectus unde est dolorem. Distinctio fugiat iusto ad corrupti maxime officia? Benkler studies commons-based peer production',
 
-}
+  },
 ];
 
-const speakersSection = document.querySelector(".speakers");
-const speakersDiv = document.createElement("div");
-speakersDiv.classList.add("speakers-div");
-for (let i =0; i < speakersInfos.length; i++){
-    speakersDiv.innerHTML += `
+const speakersSection = document.querySelector('.speakers');
+const speakersDiv = document.createElement('div');
+speakersDiv.classList.add('speakers-div');
+for (let i = 0; i < speakersInfos.length; i += 1) {
+  speakersDiv.innerHTML += `
     <div class="card mb-3 border-0" spk-card">
         <div class="row g-0 mob-spk-div">
             <div class="col-md-4 border-0 spk-img-cont">
@@ -82,7 +81,6 @@ for (let i =0; i < speakersInfos.length; i++){
             </div>
         </div>
     </div>
-`
-};
-console.log("hello")
+`;
+}
 speakersSection.appendChild(speakersDiv);
